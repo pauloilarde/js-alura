37af769df157cc88f85eba4924b095b15dbe86af
Paulo <pauloilarde@gmail.com>
2017-06-27 15:42:19 -0300

    Alterações feitas na aula 02

--- introducao-javascript/js/main.js
+++ introducao-javascript/js/main.js
@@ -1,4 +1,18 @@
-console.log("Fui carregado de um arquivo externo.");
-
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
+
+console.log(imc);
