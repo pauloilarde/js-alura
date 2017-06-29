3075b22f8c540c21ffdbcc7992caf5373a939e17
Paulo <pauloilarde@gmail.com>
2017-06-29 13:05:29 -0300

    Aula 07

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -11,19 +11,19 @@ botaoAdicionar.addEventListener("click", function (event) {
 
    var erros = validaPaciente(paciente);
 
+   console.log(erros);
    if(erros.length > 0){
-      var mensagemErro = document.querySelector("#mensagem-erro");
-      mensagemErro.textContent = erros;
+      exibeMensagensDeErro(erros);
       return;
    }
-
-   console.log(paciente);
 
    var tabela = document.querySelector("#tabela-pacientes");
 
    tabela.appendChild(pacienteTr);
 
    form.reset();
+   var mensagensDeErro = document.querySelector("#mensagens-erro");
+   mensagensDeErro.innerHTML = "";
 
 });
 
@@ -75,11 +75,38 @@ function montaTd(dado, classe) {
 function validaPaciente(paciente) {
    var erros = [];
 
+   if(paciente.nome.length == 0){
+      erros.push("O nome não pode ser em branco!");
+   }
+
    if(!validaPeso(paciente.peso)){
       erros.push("Peso é inválido!");
    }
    if(!validaAltura(paciente.altura)){
       erros.push("Altura é inválida!");
    }
+
+   if(paciente.gordura.length == 0){
+      erros.push("A gordura não pode ser em branco!");
+   }
+
+   if(paciente.altura.length == 0){
+      erros.push("A altura não pode ser em branco!");
+   }
+
+   if(paciente.peso.length == 0){
+      erros.push("O peso não pode ser em branco!");
+   }
    return erros;
+}
+
+function exibeMensagensDeErro(erros){
+   var ul = document.querySelector("#mensagens-erro");
+   ul.innerHTML = "";
+
+   erros.forEach(function(erro){
+      var li = document.createElement("li");
+      li.textContent = erro;
+      ul.appendChild(li);
+   });
 }
