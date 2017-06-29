59de36298e7476e7dac43d046cd329c9c15d26a6
Paulo <pauloilarde@gmail.com>
2017-06-29 13:28:13 -0300

    Removendo a linha inteira

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -1,8 +1,16 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function (paciente) {
-   paciente.addEventListener("dblclick", function(){
-      console.log("Fui clicado com duplo clique");
-      this.remove();
-   });
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(){
+   var alvoEvento = event.target;
+   var paiDoAlvo = alvoEvento.parentNode;
+   paiDoAlvo.remove();
 });
+
+// pacientes.forEach(function (paciente) {
+//    paciente.addEventListener("dblclick", function(){
+//       console.log("Fui clicado com duplo clique");
+//       this.remove();
+//    });
+// });
